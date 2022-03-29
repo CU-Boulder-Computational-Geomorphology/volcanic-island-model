53c92eb982f4cb991e485ca6320363ec696eca4a
Greg Tucker <gtucker@colorado.edu>
2022-03-29 13:05:35 -0600

    initial src commit

---
+++ src/volcanic_island_model.py
@@ -0,0 +1,63 @@
+"""
+volcanic_island_model.py
+
+Simulation model of an eroding volcanic island. Team-built by
+CU Boulder GEOL5702 group, Spring semester 2022.
+"""
+
+
+class VolcanicIslandSimulator:
+
+    def __init__(self, params={}):
+        """Initialize VolcanicIslandSimulator"""
+
+        # Parse general simulation parameters, create common fields, and
+        # initialize
+
+        # Create grid
+
+        # Set up initial conditions
+
+        # For each process/phenomenon, parse parameters, create field(s),
+        # instantiate components, and perform other initialization
+
+        #   sea level and/or tectonics
+
+        #   lithosphere flexure?
+
+        #   hillslope weathering and transport
+
+        #   precipitation
+
+        #   flow routing
+
+        #   fluvial erosion, transport, deposition
+
+        #   submarine sediment transport
+
+        #   submarine carbonate production
+
+        pass
+
+    def update(self):
+        """Update simulation for one global time step"""
+
+        # Update tectonics and/or sea level
+
+        # Apply weathering and hillslope transport
+
+        # Update precipitation
+
+        # Update flow routing
+
+        # Apply fluvial erosion, transport, and deposition
+
+        # Apply submarine sediment transport
+
+        # Produce marine carbonate
+
+        pass
+
+    def run(self):
+        """Run simulation from start to finish"""
+        pass
